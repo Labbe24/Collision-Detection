1303813b70d782e3ef5717854010650682e53fb4
au616728@uni.au.dk <au616728@uni.au.dk>
2022-05-09 10:19:50 +0200

    movable camera

---
+++ RobotVisualizationAndCollisionDetection/Assets/CameraMovementScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour
+{
+    private float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+     {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+        transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+        transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+        transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+        transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
+        }
+    }
+}
